e2597d50b79a788e2cd420e7183d49007d20b9aa
Mohammed Thoufic <lmsdev10@nipponpaint.co.in>
2023-03-09 15:18:33 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Layout from "./components/Layout"
+import nipponlogo from "../src/assets/download.webp"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={nipponlogo} alt='nippon logo' style={{positiob: 'relative', width: '50px', height: '50px', right: '100px'}}/>
+        Header
       </header>
+      <Layout />
+      <footer className="App-footer">
+        Footer
+      </footer>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>
+      <div >
+      <div className="mainHeader" >
+        <h2>React Js onScroll Sticky Header Example</h2>
+      </div>
+      <div className="main-block">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          lacinia pretium erat ac rutrum.
+        </p>
+      
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default layout
